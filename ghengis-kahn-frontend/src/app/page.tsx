6753cdcb4f4011147c7eaaf37a513f71f30b2634
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2025-01-24 14:33:53 -0500

    youtube embed added

--- ghengis-kahn-frontend/src/app/page.tsx
+++ ghengis-kahn-frontend/src/app/page.tsx
@@ -113,10 +113,10 @@ export default function Home() {
 								height="315"
 								src="https://www.youtube.com/embed/dwNoImzJuUs?si=ugP3563wsqudUMTe"
 								title="YouTube video player"
-								frameborder="0"
+								frameBorder="0"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-								referrerpolicy="strict-origin-when-cross-origin"
-								allowfullscreen
+								referrerPolicy="strict-origin-when-cross-origin"
+								allowFullScreen
 							></iframe>
 						</section>
 						<section className="bg-black/70 text-white rounded-lg shadow-lg p-8 max-w-4xl">
