339dec1b7e92838c7cf2bf14804d17781fcb173e
Jordan Rein <jordan@basilica.digital>
2025-01-26 19:30:11 -0800

    New theme

--- ghengis-kahn-frontend/apollo-client.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-	uri: "https://api.studio.thegraph.com/query/45871/genghis-kahn-ai-token/version/latest",
-	cache: new InMemoryCache(),
-});
-
-export default client;
