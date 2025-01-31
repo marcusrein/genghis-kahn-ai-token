56de49499223102894c7ec469a9dff80bab3282f
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2025-01-31 05:56:11 -0800

    Add link to whitepaper in Hero component

--- ghengis-kahn-frontend/components/hero.tsx
+++ ghengis-kahn-frontend/components/hero.tsx
@@ -124,6 +124,17 @@ export default function Hero() {
 									<span>YouTube</span>
 								</a>
 							</div>
+
+							<div>
+								<a
+									className="btn text-slate-200 hover:text-white bg-slate-900 bg-opacity-25 hover:bg-opacity-30 w-full transition duration-150 ease-in-out"
+									href="https://aiagent.gitbook.io/genghiskahnai"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<span>Whitepaper</span>
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
